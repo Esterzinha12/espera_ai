04d0ed46919b9206227514a4dbac1aa26ed28dda
Ester Girelli <ester_girelli@estudante.sesisenai.org.br>
2024-05-29 20:29:58 -0300

    ok

--- espera_ai-master/src/pages/Home/Home.js
+++ espera_ai-master/src/pages/Home/Home.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Navbar} from "../../components/Navbar";
+import axios from "axios";
 
 export function Home() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try{
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setPokemon(response.data.results);
+        console.log(pokemon);
+      } catch (error){
+        console.log(error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
   return (
-    <>
-    <Navbar/>
-    </>
+    <div>
+      <h1>Pokemon</h1>
+      <ul>
+        {
+          pokemon.map((poke) => {
+            return <li>{poke.name}</li>
+          })
+        }
+      </ul>
+    </div>
   )
 }
